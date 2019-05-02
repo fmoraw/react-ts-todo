1f4080daca6624e0bed1f5c69fc1013cea6378b2
Elina Schäfer <elfgaben@Elinas-MacBook-Pro.local>
2019-05-02 13:58:14 +0200

    Add Parse to application

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import * as Parse from 'parse';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import ListComponent from "./02-ListComponent/TodoList"
 import SandboxHeader from './SandBoxHeader';
+
+declare var process : {
+  env: {
+    REACT_APP_PARSE_APP_ID: string,
+    REACT_APP_PARSE_SERVER_URL: string,
+  }
+}
+
+const initializeParse = () => {
+  Parse.initialize(process.env.REACT_APP_PARSE_APP_ID);
+  (Parse as any).serverURL = process.env.REACT_APP_PARSE_SERVER_URL;
+};
+
+initializeParse();
 
 const App: React.FC = () => {
   return (
