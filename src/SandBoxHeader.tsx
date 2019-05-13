79e3491488a84c1a32fb2bf7a07a623ba0679c09
Elina Schäfer <elfgaben@Elinas-MacBook-Pro.local>
2019-05-13 11:42:45 +0200

    add folders for step 1, 6 and 8

--- src/SandBoxHeader.tsx
+++ src/SandBoxHeader.tsx
@@ -10,10 +10,12 @@ export default class SandboxHeader extends React.Component<{},SandboxHeaderState
     public render() {
         return(
             <React.Fragment>
-     
-                <a href="#/0">Next</a>
+                <a href="#/2">Next</a>
                 <a href="#/1">Previous</a>
                 <a href="#/solution">Solution</a>
+                <a href="#/from0ToReact">Schritt 1: From 0 to React</a>
+                <a href="#/backend">Schritt 6: Todos fetchen</a>
+                <a href="#/forms">Schritt 8: Formulare mit redux-form</a>
             </React.Fragment>
         )
     }
--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,9 @@ import SandboxHeader from './SandBoxHeader';
 
 import { store, history } from "./02-ListComponent/setupMiddleware"
 import ListComponentContainer from './02-ListComponent/ListComponent.container';
+import BackendContainer from './backend/ListComponent.container';
+import FormContainer from './forms/ListComponent.container';
+import From0ToReactContainer from './from0toReact/ListComponent.container';
 
 const initializeParse = () => {
     Parse.initialize('myAppId');
@@ -25,6 +28,9 @@ ReactDOM.render(
         <HashRouter>
             <Switch>
                 <Route path="/0"><ListComponentContainer/></Route>
+                <Route path="/from0ToReact"><From0ToReactContainer/></Route>
+                <Route path="/backend"><BackendContainer/></Route>
+                <Route path="/forms"><FormContainer/></Route>
                 <Route path="/1"><div>Test</div></Route>
             </Switch>
         </HashRouter>
